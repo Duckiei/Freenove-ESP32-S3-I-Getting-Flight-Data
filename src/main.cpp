3a5695950f3215ceff136290c9d661cdd638ff4f
Duckiei <shayaan.shahid@Hotmail.com>
2026-08-23 20:03:11 -0400

    Code Cleaned up

--- src/main.cpp
+++ src/main.cpp
@@ -9,121 +9,136 @@
 #include <vector>
 #include <Plane_Icon_30x30px_TrueColourAlpha.h>
 #include "tokens.h"
+#include <Esp.h>
 
-//========= Initialization =========\\
+//=========Initialization =========\\
 
+// Wifi
 const String ssid = WIFI_SSID;
 const String pass = WIFI_PASSWORD;
 
+// Bounding Box
 const float lamin = 43.5;
 const float lomin = -79.898071;
 const float lamax = 44.276;
 const float lomax = -78.733521;
 
+// Screen dimensions
 const int SCREEN_WIDTH = 480;
 const int SCREEN_HEIGHT = 320;
 
-std::vector<lv_obj_t> squares;
-lv_obj_t *label;
+// Hold all plane obj's
+std::vector<lv_obj_t> planes;
+
+// Object initialization
+lv_obj_t *callsign_label;
+
+// LVGL --> Display wrapper
 Display screen;
 
-float mapFloat(float start, float fromLow, float fromMax, float toLow, float toMax);
-
-void btn_event_cb(lv_event_t *e)
+// Struct
+struct Plane
 {
-  lv_obj_t *obj = lv_event_get_target(e);
-  const char *callsign = (const char *)lv_obj_get_user_data(obj);
-  lv_label_set_text(label, callsign);
-}
+  String callsign;
+  String origin_country;
+  float longitude;
+  float latitude;
+  float baro_altitude;
+  bool on_ground;
+  float velocity;
+  float heading;
+  float vertical_rate;
+  int category;
+};
+
+// Methods Initialization
+float mapFloat(float start, float fromLow, float fromMax, float toLow, float toMax);
+void btn_event_cb(lv_event_t *e);
+lv_obj_t *buildplaneScreen();
+void buildPlane(Plane &planeData, lv_obj_t *planeScreen);
 
 void setup()
 {
-  // put your setup code here, to run once:
+  screen.init();
   Serial.begin(9600);
   delay(1750);
 
-  screen.init();
   lv_obj_set_style_bg_color(lv_scr_act(), lv_color_hex(0x181a26), 0);
-  label = lv_label_create(lv_scr_act());
-  lv_label_set_text(label, "Callsign: ");
-  lv_obj_align_to(label, lv_scr_act(), LV_ALIGN_TOP_LEFT, 20, 20);
-  lv_obj_set_style_text_color(label, lv_color_hex(0xffffff), 0);
-  lv_obj_set_style_text_font(label, &lv_font_montserrat_20, 0);
 
-  Serial.println("Connecting...");
+  // Connect to Wifi
+  Serial.println("Connecting WiFi...");
   WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(5);
   }
-
   Serial.println(WiFi.localIP());
 
+  // Handle encryption
   WiFiClientSecure client;
   client.setInsecure();
 
+  // Build HTTPS Url
   HTTPClient https;
   String url = "https://opensky-network.org/api/states/all?";
   url += "lamin=" + String(lamin);
   url += "&lomin=" + String(lomin);
   url += "&lamax=" + String(lamax);
   url += "&lomax=" + String(lomax);
-
   Serial.println(url);
+
+  // Check if connection to url can be made
   if (https.begin(client, url))
   {
+    // Retrieve response code & print it
     int httpCode = https.GET();
     Serial.printf("Http Code: %d \n", httpCode);
+
+    // Validate connection can even be made
     if (httpCode > 0)
     {
-      Serial.printf("HTTP response code: %d\n", httpCode);
-
+      // Validate connection is proper
       if (httpCode == HTTP_CODE_OK)
       {
-        String payload = https.getString();
         JsonDocument doc;
 
+        // Retrieve JSON & parse it
+        String payload = https.getString();
         deserializeJson(doc, payload);
 
-        JsonArray states = doc["states"].as<JsonArray>();
+        // Explicitly convert json into accessible array
+        JsonArray planeStates = doc["states"].as<JsonArray>();
 
-        for (JsonArray state : states)
+        lv_obj_t *planeScreen = buildplaneScreen();
+
+        // Create all plane objects
+        for (JsonArray planeState : planeStates)
         {
-          if (!state[8])
+          Plane plane;
+
+          plane.callsign = planeState[1].as<String>();
+          plane.origin_country = planeState[2].as<String>();
+          plane.longitude = planeState[5];
+          plane.latitude = planeState[6];
+          plane.baro_altitude = planeState[7];
+          plane.on_ground = planeState[8];
+          plane.velocity = planeState[9];
+          plane.heading = planeState[10];
+          plane.vertical_rate = planeState[11];
+          plane.category = planeState[17];
+
+          if (!plane.on_ground)
           {
-            const char *callsign = state[1];
-            float lat = state[6];
-            float lon = state[5];
-            int heading = state[10];
-
-            float lat_mapped = mapFloat(lat, lamin, lamax, SCREEN_HEIGHT, 0);
-            float lon_mapped = mapFloat(lon, lomin, lomax, 0, SCREEN_WIDTH);
-
-            lv_obj_t *square = lv_img_create(lv_scr_act());
-            lv_img_set_src(square, &Plane_Icon_30x30px);
-            lv_obj_add_flag(square, LV_OBJ_FLAG_CLICKABLE);
-
-            lv_obj_set_pos(square, lon_mapped - 15, lat_mapped - 15);
-            lv_obj_set_style_img_recolor(square, lv_color_hex(0xffffff), 0);
-            lv_obj_set_style_img_recolor_opa(square, LV_OPA_COVER, 0);
-            lv_img_set_angle(square, heading * 10);
-
-            char *callsignCopy = strdup(callsign);
-            lv_obj_set_user_data(square, callsignCopy);
-            lv_obj_add_event_cb(square, btn_event_cb, LV_EVENT_CLICKED, NULL);
-            squares.push_back(*square);
-
-            Serial.printf("%s: %.4f, %.4f\n", callsign, lat_mapped, lon_mapped);
+            buildPlane(plane, planeScreen);
           }
         }
-        Serial.println("Finished printing values.");
       }
+      Serial.println("Finished printing values.");
     }
-    else
-    {
-      Serial.println("Failed to connect");
-      Serial.println("Restrying...");
-    }
+  }
+  else
+  {
+    Serial.println("Failed to connect");
   }
   https.end();
 }
@@ -135,7 +150,56 @@ void loop()
   delay(5);
 }
 
+//========= FUNCTIONS / METHODS =========\\
+
 float mapFloat(float start, float fromLow, float fromMax, float toLow, float toMax)
 {
   return (((start - fromLow) * (toMax - toLow)) / (fromMax - fromLow)) + toLow;
+}
+
+void btn_event_cb(lv_event_t *e)
+{
+  lv_obj_t *obj = lv_event_get_target(e);
+  Plane *plane = (Plane *)lv_obj_get_user_data(obj);
+  lv_label_set_text(callsign_label, plane->callsign.c_str());
+}
+
+lv_obj_t *buildplaneScreen() // return a pointer
+{
+  lv_obj_t *planeScreen = lv_obj_create(NULL);
+  lv_obj_set_style_bg_color(planeScreen, lv_color_hex(0x181a26), 0);
+
+  callsign_label = lv_label_create(planeScreen);
+  lv_label_set_text(callsign_label, "Callsign: ");
+  lv_obj_align_to(callsign_label, planeScreen, LV_ALIGN_TOP_LEFT, 20, 20);
+  lv_obj_set_style_text_color(callsign_label, lv_color_hex(0xffffff), 0);
+  lv_obj_set_style_text_font(callsign_label, &lv_font_montserrat_20, 0);
+  lv_scr_load(planeScreen);
+
+  return planeScreen; // return the actual pointer, no dereference
+}
+
+void buildPlane(Plane &planeData, lv_obj_t *planeScreen)
+{
+  lv_obj_t *plane = lv_img_create(planeScreen);
+  lv_img_set_src(plane, &Plane_Icon_30x30px);
+  lv_obj_add_flag(plane, LV_OBJ_FLAG_CLICKABLE);
+
+  float lat_mapped = mapFloat(planeData.latitude, lamin, lamax, SCREEN_HEIGHT, 0);
+  float lon_mapped = mapFloat(planeData.longitude, lomin, lomax, 0, SCREEN_WIDTH);
+
+  lv_obj_set_pos(plane, lon_mapped - 15, lat_mapped - 15);
+  lv_obj_set_style_img_recolor(plane, lv_color_hex(0xffffff), 0);
+  lv_obj_set_style_img_recolor_opa(plane, LV_OPA_COVER, 0);
+  lv_img_set_angle(plane, planeData.heading * 10);
+
+  Plane *planeCopy = new Plane(planeData); // heap-allocated copy of the whole struct
+  lv_obj_set_user_data(plane, planeCopy);
+  lv_obj_add_event_cb(plane, btn_event_cb, LV_EVENT_CLICKED, NULL);
+  planes.push_back(*plane);
+}
+
+void buildStartupScreen()
+{
+  lv_obj_t *startupScreen = lv_obj_create(NULL);
 }
