3e89a548a75a95a96dc8aa3aed99b999e148a597
Duckiei <shayaan.shahid@Hotmail.com>
2026-08-25 03:53:59 -0400

    Added dark mode screen togglable from main plane screen colours are more streamline now, no need for hex, and easier to change

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 //========= Libraries =========\\
 
 #include <Arduino.h>
+#include <lvgl.h>
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <HTTPClient.h>
@@ -11,6 +12,7 @@
 #include "tokens.h"
 #include <Esp.h>
 #include <mapbox_static_480x320_markers.h>
+#include <mapbox_static_480x320_Dark.h>
 
 //=========Initialization =========\\
 
@@ -35,6 +37,8 @@ std::vector<lv_obj_t *> planes;
 lv_obj_t *pInfoboxLabel;
 lv_obj_t *pStartupScreenText;
 String startupScreenText = "";
+lv_obj_t *planeScreen = nullptr;
+lv_obj_t *pPreviousSelectedPlane = nullptr;
 
 // LVGL --> Display wrapper
 Display screen;
@@ -54,32 +58,51 @@ struct Plane
   int category;
 };
 
+// Token & repeating stuff
 String token;
 unsigned long expires_at;
 unsigned long whenUpdateScreen;
 
-lv_obj_t *pPreviousSelectedPlane = nullptr;
+// Colours
+const int WHITE = 0xffffff;
+const int HOT_PINK = 0xff006b;
+const int YELLOW = 0xffe100;
+const int DARK_GREY = 0x202020;
+const int BLACK = 0x000000;
+
+// Colours, Backgrounds, Images
+const int planeColourDefault = WHITE;
+int planeColourSelected = YELLOW;
+const lv_img_dsc_t *mapBackground = &mapbox_static_480x320_markers;
+const int startupScreenColour = DARK_GREY;
+const int startupScreenTextColour = WHITE;
+const int infoBoxColour = BLACK;
+const int infoBoxTextColour = WHITE;
+const int arrowButtonsColour = BLACK;
+
+bool isLight = true;
 
 // Methods Initialization
 float mapFloat(float start, float fromLow, float fromMax, float toLow, float toMax);
-void btn_event_cb(lv_event_t *e);
+void user_select_plane(lv_event_t *e);
 lv_obj_t *buildplaneScreen();
 void buildPlane(Plane &planeData, lv_obj_t *planeScreen);
 lv_obj_t *buildStartupScreen();
 void getToken4000();
 void drawPlanestoScreen(lv_obj_t *planeScreen);
+lv_obj_t *buildSettingsScreen();
 
-lv_obj_t *planeScreen = nullptr;
 void setup()
 {
   // Startup
   screen.init();
   lv_obj_t *startupScreen = buildStartupScreen();
+  lv_scr_load(startupScreen);
   pStartupScreenText = lv_label_create(startupScreen);
   lv_label_set_text(pStartupScreenText, startupScreenText.c_str());
   lv_obj_align_to(pStartupScreenText, startupScreen, LV_ALIGN_TOP_MID, -90, 20);
   lv_obj_set_style_text_font(pStartupScreenText, &lv_font_montserrat_14, 0);
-  lv_obj_set_style_text_color(pStartupScreenText, lv_color_hex(0x000000), 0);
+  lv_obj_set_style_text_color(pStartupScreenText, lv_color_hex(startupScreenTextColour), 0);
   screen.routine();
   Serial.begin(9600);
   delay(1750);
@@ -110,6 +133,7 @@ void setup()
   planeScreen = buildplaneScreen();
   drawPlanestoScreen(planeScreen);
   lv_scr_load(planeScreen);
+  lv_obj_del(startupScreen);
 }
 
 void loop()
@@ -151,7 +175,7 @@ float mapFloat(float start, float fromLow, float fromMax, float toLow, float toM
   return (((start - fromLow) * (toMax - toLow)) / (fromMax - fromLow)) + toLow;
 }
 
-void btn_event_cb(lv_event_t *e)
+void user_select_plane(lv_event_t *e)
 {
   // Get data
   lv_obj_t *planeObj = lv_event_get_target(e);
@@ -160,11 +184,11 @@ void btn_event_cb(lv_event_t *e)
   // Colouring highlighted plane
 
   // Set new plane colour
-  lv_obj_set_style_img_recolor(planeObj, lv_color_hex(0xffe100), 0);
+  lv_obj_set_style_img_recolor(planeObj, lv_color_hex(planeColourSelected), 0);
   if (pPreviousSelectedPlane != nullptr && pPreviousSelectedPlane != planeObj)
   {
     // Set previously selected plane white if its a diffferent plane and its actually something
-    lv_obj_set_style_img_recolor(pPreviousSelectedPlane, lv_color_hex(0xffffff), 0);
+    lv_obj_set_style_img_recolor(pPreviousSelectedPlane, lv_color_hex(planeColourDefault), 0);
   }
 
   pPreviousSelectedPlane = planeObj;
@@ -177,15 +201,35 @@ void btn_event_cb(lv_event_t *e)
   lv_label_set_text(pInfoboxLabel, (callsign + "\n" + origin_country + "\n" + baro_altitude * 3.281 + " ft \n" + velocity * 1.944 + " kts").c_str());
 }
 
+void toggleViewMode(lv_event_t *e)
+{
+  if (isLight)
+  {
+    mapBackground = &mapbox_static_480x320_Dark;
+    planeColourSelected = HOT_PINK;
+  }
+  else
+  {
+    mapBackground = &mapbox_static_480x320_markers;
+    planeColourSelected = YELLOW;
+  }
+
+  lv_obj_set_style_bg_img_src(planeScreen, mapBackground, 0);
+
+  isLight = !isLight;
+}
+
 lv_obj_t *buildplaneScreen()
 {
+  // Screen itself
   lv_obj_t *planeScreen = lv_obj_create(NULL);
-  lv_obj_set_style_bg_img_src(planeScreen, &mapbox_static_480x320_markers, 0);
+  lv_obj_set_style_bg_img_src(planeScreen, mapBackground, 0);
+  lv_obj_clear_flag(planeScreen, LV_OBJ_FLAG_SCROLLABLE);
 
   // Infobox
   lv_obj_t *infoBox = lv_obj_create(planeScreen);
   lv_obj_align_to(infoBox, planeScreen, LV_ALIGN_TOP_LEFT, 20, 20);
-  lv_obj_set_style_bg_color(infoBox, lv_color_hex(0x000000), 0);
+  lv_obj_set_style_bg_color(infoBox, lv_color_hex(infoBoxColour), 0);
   lv_obj_set_style_opa(infoBox, LV_OPA_50, 0);
   lv_obj_set_size(infoBox, 200, 90);
 
@@ -193,16 +237,39 @@ lv_obj_t *buildplaneScreen()
   pInfoboxLabel = lv_label_create(planeScreen);
   lv_label_set_text(pInfoboxLabel, "Select a \nplane for \ndata: ");
   lv_obj_align_to(pInfoboxLabel, planeScreen, LV_ALIGN_TOP_LEFT, 30, 30);
-  lv_obj_set_style_text_color(pInfoboxLabel, lv_color_hex(0xffffff), 0);
+  lv_obj_set_style_text_color(pInfoboxLabel, lv_color_hex(infoBoxTextColour), 0);
   lv_obj_set_style_text_font(pInfoboxLabel, &lv_font_montserrat_14, 0);
 
-  lv_obj_clear_flag(planeScreen, LV_OBJ_FLAG_SCROLLABLE);
+  // Corner arrow buttons
+  lv_obj_t *pRightArrow = lv_btn_create(planeScreen);
+  lv_obj_t *pLeftArrow = lv_btn_create(planeScreen);
+  lv_obj_set_size(pRightArrow, 40, 40);
+  lv_obj_set_size(pLeftArrow, 40, 40);
 
-  // lv_obj_t *pDiagnosticButton = lv_btn_create(lv_scr_act());
-  // lv_obj_align_to(pDiagnosticButton, lv_scr_act(), LV_ALIGN_TOP_RIGHT, -20, 20);
-  // lv_obj_add_event_cb(pDiagnosticButton, );
+  lv_obj_align(pRightArrow, LV_ALIGN_TOP_RIGHT, -10, 20);
+  lv_obj_align(pLeftArrow, LV_ALIGN_TOP_RIGHT, -55, 20);
 
-  return planeScreen; // return the actual pointer, no dereference
+  lv_obj_set_style_bg_color(pRightArrow, lv_color_hex(arrowButtonsColour), 0);
+  lv_obj_set_style_bg_color(pLeftArrow, lv_color_hex(arrowButtonsColour), 0);
+
+  lv_obj_set_style_opa(pRightArrow, LV_OPA_50, 0);
+  lv_obj_set_style_opa(pLeftArrow, LV_OPA_50, 0);
+
+  lv_obj_t *rightArrowLabel = lv_label_create(pRightArrow);
+  lv_obj_t *leftArrowLabel = lv_label_create(pLeftArrow);
+
+  lv_obj_align(rightArrowLabel, LV_ALIGN_CENTER, 0, 0);
+  lv_obj_align(leftArrowLabel, LV_ALIGN_CENTER, 0, 0);
+
+  lv_label_set_text(rightArrowLabel, LV_SYMBOL_SETTINGS);
+  lv_label_set_text(leftArrowLabel, LV_SYMBOL_EYE_OPEN);
+
+  lv_obj_set_style_text_color(rightArrowLabel, lv_color_hex(WHITE), 0);
+  lv_obj_set_style_text_color(leftArrowLabel, lv_color_hex(WHITE), 0);
+
+  lv_obj_add_event_cb(pLeftArrow, toggleViewMode, LV_EVENT_CLICKED, NULL);
+
+  return planeScreen;
 }
 
 void buildPlane(Plane &planeData, lv_obj_t *planeScreen)
@@ -215,21 +282,20 @@ void buildPlane(Plane &planeData, lv_obj_t *planeScreen)
   float lon_mapped = mapFloat(planeData.longitude, lomin, lomax, 0, SCREEN_WIDTH);
 
   lv_obj_set_pos(plane, lon_mapped - 15, lat_mapped - 15);
-  lv_obj_set_style_img_recolor(plane, lv_color_hex(0xffffff), 0);
+  lv_obj_set_style_img_recolor(plane, lv_color_hex(planeColourDefault), 0);
   lv_obj_set_style_img_recolor_opa(plane, LV_OPA_COVER, 0);
   lv_img_set_angle(plane, planeData.heading * 10);
 
   Plane *planeCopy = new Plane(planeData); // heap-allocated copy of the whole struct
   lv_obj_set_user_data(plane, planeCopy);
-  lv_obj_add_event_cb(plane, btn_event_cb, LV_EVENT_CLICKED, NULL);
+  lv_obj_add_event_cb(plane, user_select_plane, LV_EVENT_CLICKED, NULL);
   planes.push_back(plane);
 }
 
 lv_obj_t *buildStartupScreen()
 {
   lv_obj_t *startupScreen = lv_obj_create(NULL);
-  lv_obj_set_style_bg_color(startupScreen, lv_color_hex(0xffffff), 0);
-  lv_scr_load(startupScreen);
+  lv_obj_set_style_bg_color(startupScreen, lv_color_hex(startupScreenColour), 0);
   return startupScreen;
 }
 
@@ -301,12 +367,15 @@ void drawPlanestoScreen(lv_obj_t *planeScreen)
 
   https.begin(client, url);
   https.addHeader("Authorization", "Bearer " + token);
+  const char *keys[] = {"X-Rate-Limit-Remaining"};
+  https.collectHeaders(keys, 1);
   int httpCode = https.GET();
 
   // Validate connection is proper
   if (httpCode == HTTP_CODE_OK)
   {
     // Retrieve JSON & parse it
+    String rateLimitRemaining = https.header("X-Rate-Limit-Remaining");
     String payload = https.getString();
     deserializeJson(doc, payload);
 
@@ -339,9 +408,28 @@ void drawPlanestoScreen(lv_obj_t *planeScreen)
         buildPlane(plane, planeScreen);
       }
     }
+    Serial.println("Rate limit remaining: " + rateLimitRemaining);
   }
   whenUpdateScreen = millis() / 1000 + 30;
   pPreviousSelectedPlane = nullptr;
   doc.clear();
   https.end();
+}
+
+lv_obj_t *buildSettingsScreen()
+{
+  lv_obj_t *screen = lv_obj_create(NULL);
+  // Update all this when the panel is switched to, and when refresh button is pressed
+
+  // free heap
+
+  // Wifi status, ip rssi
+
+  // api calls remaining
+
+  // uptime (update millis label when the button is pressed)
+
+  // flash space
+
+  return screen;
 }
